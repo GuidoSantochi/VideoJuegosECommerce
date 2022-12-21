d40fecf769e7cb175ad8c053b1025bf8c9fb9d45
GuidoSantochi <guidosanto201080@hotmail.com>
2022-12-21 20:41:13 -0300

    primer commit
    agregue index,css y js

---
+++ js/products.js
@@ -0,0 +1,26 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Age of Empires II",
+    precio: 50,
+    img: "../assets/ageII.png",
+  },
+  {
+    id: 2,
+    nombre: "Doom",
+    precio: 100,
+    img: "../assets/doom.png",
+  },
+  {
+    id: 3,
+    nombre: "Minecraft",
+    precio: 150,
+    img: "../assets/mc.png",
+  },
+  {
+    id: 4,
+    nombre: "Elden Ring",
+    precio: 200,
+    img: "../assets/eldenring.png",
+  },
+];
