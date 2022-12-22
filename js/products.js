3dd2e33c54c903497ef31746350240435689258c
GuidoSantochi <guidosanto201080@hotmail.com>
2022-12-22 17:23:13 -0300

    Agregue contador al carrito

--- js/products.js
+++ js/products.js
@@ -4,23 +4,27 @@ const productos = [
     nombre: "Age of Empires II",
     precio: 50,
     img: "../assets/ageII.png",
+    cantidad: 1,
   },
   {
     id: 2,
     nombre: "Doom",
     precio: 100,
     img: "../assets/doom.png",
+    cantidad: 1,
   },
   {
     id: 3,
     nombre: "Minecraft",
     precio: 150,
     img: "../assets/mc.png",
+    cantidad: 1,
   },
   {
     id: 4,
     nombre: "Elden Ring",
     precio: 200,
     img: "../assets/eldenring.png",
+    cantidad: 1,
   },
 ];
